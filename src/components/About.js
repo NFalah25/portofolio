9652449e25fc62aa83791b7c141e7d93ef1f547d
NFalah25 <falahnaufal1@gmail.com>
2024-07-10 16:24:59 +0700

    perbaikan

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,7 @@ function About() {
 				<div className=''>
 					<Image
 						src={naufal}
+						alt='Picture of Naufal'
 						height={450}
 						className='rounded-full'
 					/>
@@ -18,12 +19,12 @@ function About() {
 				<div className='md:w-1/3 text-[1.1em] text-justify md:leading-10 mx-5 md:mx-0'>
 					I am an Informatics Engineering student from Malang State
 					Polytechnic who wants to increase knowledge and experience in
-					various fields. I am very interested in web developer, mobile
+					various fields. I am very interested in web development, mobile
 					applications, and UI/UX design. I am a child who is always on
-					time and doesn't waste time because time is important. Apart from
+					time and doesn&apos;t waste time because time is important. Apart from
 					that, I am a child who has good public speaking skills, the
-					ability to work together in a team and the enthusiasm to continue
-					learning
+					ability to work together in a team, and the enthusiasm to continue
+					learning.
 				</div>
 			</div>
 		</Template>
